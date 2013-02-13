650ea107489f5b6c623bd5f59fb029d3e826c016
David Coss <David.Coss@stjude.org>
2013-02-13 10:53:02 -0600

    Added a names function that maps column names to column indices for data files.

--- python/array_tools.py
+++ python/array_tools.py
@@ -22,3 +22,19 @@ def rmse(arr):
     avg = np.mean(arr)
     return sqrt(np.mean((arr-avg)**2))
     
+def names(_file,delimiter = '\t'):
+    """
+    Takes a file and maps names in the header line to column indices (zero-indexed)
+    """
+
+    infile = _file
+    if isinstance(_file,str):
+        infile = open(_file,"r")
+    header = infile.readline()
+    tokens = header.split(delimiter)
+    idx = 0
+    names = {}
+    for token in tokens:
+        names[token] = idx
+        idx += 1
+    return names

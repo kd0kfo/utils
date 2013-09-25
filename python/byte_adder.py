d0e20f67e96d79485bfe6bc52525f8069a523d9c
David Coss <David.Coss@stjude.org>
2013-09-25 10:18:43 -0500

    Created a script to sum file sizes.

---
+++ python/byte_adder.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Takes a list of file sizes and adds them.
+# Returns the results in bytes.
+
+from sys import argv, stdin, stdout
+from getopt import getopt
+
+KNOWN_UNITS = {"k": 1024, "m": 1048576, "g": 1073741824, "t": 1099511627776}
+
+
+def get_conversion_factor(units):
+    if not units in KNOWN_UNITS:
+        raise Exception("Unkown Units: {0}".format(units))
+
+    return KNOWN_UNITS[units]
+
+
+infile = stdin
+outfile = stdout
+
+short_opts = "i:o:"
+long_opts = ["input=", "output="]
+
+(opts, args) = getopt(argv[1:], short_opts, long_opts)
+
+for (opt, optargs) in opts:
+    while opt[0] == '-':
+        opt = opt[1:]
+
+    if opt in ["i", "input"]:
+        infile = open(optargs, "r")
+    elif opt in ["o", "output"]:
+        outfile = open(optargs, "w")
+
+total_in_bytes = 0
+for line in infile:
+    val = line.strip()
+
+    # If it's already a number, i.e. no units attached,
+    # simply add and continue
+    if val.isdigit():
+        total_in_bytes += int(val)
+        continue
+
+    # Get units and convert number to bytes
+    unit = val[-1].lower()
+    val = int(val[0:-1])
+
+    factor = get_conversion_factor(unit)
+
+    total_in_bytes += factor*val
+
+print(total_in_bytes)
